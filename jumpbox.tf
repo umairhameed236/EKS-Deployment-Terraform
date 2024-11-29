3bcccc447c7f35eb072a429d6ea2b8fc2011ca1d
umairhameed236 <101642844+umairhameed236@users.noreply.github.com>
2024-11-29 19:36:13 +0500

    Delete jumpbox.tf

--- jumpbox.tf
+++
@@ -1,8 +0,0 @@
-# module "bastion_host" {
-#   source         = "./modules/aws-ec2"
-#   instance_type  = local.instance_type
-#   key_name       = local.key_name
-#   public_subnet = local.vpc_public_subnet_cidr_block
-#  vpc_id = module.vpc.vpc_id
-# security_group = "${local.cluster_name}-sg"
-# }
